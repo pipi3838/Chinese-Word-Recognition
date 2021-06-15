a2671ec0580f232ca2c2c34be1963d06afb3a74f
pipi3838 <b04902081@ntu.edu.tw>
2021-06-15 05:06:05 +0000

    change ensemble models

--- api2.py
+++ api2.py
@@ -17,7 +17,7 @@ import torch.nn as nn
 from torch.utils.data import DataLoader, Dataset
 import torchvision.transforms as transforms
 
-from models import Densenet
+# from models import Densenet
 from efficientnet_pytorch import EfficientNet
 
 
@@ -32,15 +32,15 @@ seed = 5487
 device = 'cuda:0'
 if not torch.cuda.is_available(): device='cpu'
 
-with open('./word2label.json', 'r', encoding='utf-8') as f:
-    word2label = json.load(f)
-label2word = {val:key for key, val in word2label.items()}
+# with open('./word2label.json', 'r', encoding='utf-8') as f:
+#     word2label = json.load(f)
+# label2word = {val:key for key, val in word2label.items()}
 
-label2word2 = dict()
-with open('./idx2class.txt', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-    for idx, l in enumerate(lines):
-        label2word2[idx] = l.strip()
+# label2word2 = dict()
+# with open('./idx2class.txt', 'r', encoding='utf-8') as f:
+#     lines = f.readlines()
+#     for idx, l in enumerate(lines):
+#         label2word2[idx] = l.strip()
 
 label2word3 = dict()
 with open('./idx2class_revised.dat', 'r', encoding='utf-8') as f:
@@ -48,14 +48,14 @@ with open('./idx2class_revised.dat', 'r', encoding='utf-8') as f:
     for idx, l in enumerate(lines):
         label2word3[idx] = l.strip()
 
-first_model = '{2}_loss.pth'
-second_model = 'gridmask.pt'
+# first_model = '{2}_loss.pth'
+second_model = 'gridmask2.pt'
 # third_model = 'efficientnet_b3.pt'
-# forth_model = 'affine.pt'
+forth_model = 'affine.pt'
 
-model1 = Densenet(len(word2label)).to(device)
-model1.load_state_dict(torch.load(first_model, map_location=torch.device('cpu')))
-model1.eval()       
+# model1 = Densenet(len(word2label)).to(device)
+# model1.load_state_dict(torch.load(first_model, map_location=torch.device('cpu')))
+# model1.eval()       
 
 model2 = EfficientNet.from_name('efficientnet-b3')
 model2._fc = nn.Linear(1536, 800)
@@ -67,50 +67,51 @@ model2.eval()
 # model3.load_state_dict(torch.load(third_model, map_location=torch.device('cpu')))
 # model3.eval()
 
-# model4 = EfficientNet.from_name('efficientnet-b3')
-# model4._fc = nn.Linear(1536, 800)
-# model4.load_state_dict(torch.load(forth_model, map_location=torch.device('cpu')))
-# model4.eval()
+model4 = EfficientNet.from_name('efficientnet-b3')
+model4._fc = nn.Linear(1536, 800)
+model4.load_state_dict(torch.load(forth_model, map_location=torch.device('cpu')))
+model4.eval()
 
 softmax = nn.Softmax(dim=1)
 
-def preprocess_img(img):
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    _, thre_img = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
-    thre_img = cv2.fastNlMeansDenoising(thre_img, h=13, searchWindowSize=7)
+# def preprocess_img(img):
+#     img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+#     _, thre_img = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+#     thre_img = cv2.fastNlMeansDenoising(thre_img, h=13, searchWindowSize=7)
     
-    thre_img = cv2.fastNlMeansDenoising(thre_img, h=13, searchWindowSize=7)
-    kernel = np.ones((3,3), np.uint8)
-    thre_img = cv2.erode(thre_img, kernel, iterations = 1)
+#     thre_img = cv2.fastNlMeansDenoising(thre_img, h=13, searchWindowSize=7)
+#     kernel = np.ones((3,3), np.uint8)
+#     thre_img = cv2.erode(thre_img, kernel, iterations = 1)
 
-    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (13, 13))
-    dilate = cv2.dilate(thre_img, rect_kernel, iterations = 1)
+#     rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (13, 13))
+#     dilate = cv2.dilate(thre_img, rect_kernel, iterations = 1)
     
-    contours, hierarchy = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    if len(contours) == 0: bound = cv2.resize(img, (50,50))
-    else:
-        max_area = -1
-        bx, by, bw, bh = None, None, None, None
-        for cnt in contours:
-            x, y, w, h = cv2.boundingRect(cnt)
-            if w * h > max_area:
-                bx, by, bw, bh = x, y, w, h
-                max_area = w * h
+#     contours, hierarchy = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+#     if len(contours) == 0: bound = cv2.resize(img, (50,50))
+#     else:
+#         max_area = -1
+#         bx, by, bw, bh = None, None, None, None
+#         for cnt in contours:
+#             x, y, w, h = cv2.boundingRect(cnt)
+#             if w * h > max_area:
+#                 bx, by, bw, bh = x, y, w, h
+#                 max_area = w * h
 
-        bound = cv2.resize(img[by:by+bh, bx:bx+bw], (50, 50))
+#         bound = cv2.resize(img[by:by+bh, bx:bx+bw], (50, 50))
     
-    _, bound = cv2.threshold(bound, 0, 255, cv2.THRESH_OTSU)
-    bound = cv2.cvtColor(bound, cv2.COLOR_GRAY2BGR)
+#     _, bound = cv2.threshold(bound, 0, 255, cv2.THRESH_OTSU)
+#     bound = cv2.cvtColor(bound, cv2.COLOR_GRAY2BGR)
 
-    transform = transforms.Compose([
-        transforms.ToPILImage(),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
-    bound = transform(bound)
-    return bound
+#     transform = transforms.Compose([
+#         transforms.ToPILImage(),
+#         transforms.ToTensor(),
+#         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+#     ])
+#     bound = transform(bound)
+#     return bound
 
 def preprocess3_img(img):
+    img = img[:,:,::-1]
     transform = transforms.Compose([
         transforms.ToPILImage(),
         transforms.CenterCrop(224),
@@ -215,16 +216,16 @@ def inference():
     server_uuid = generate_server_uuid(CAPTAIN_EMAIL + ts)
     cv2.imwrite('./image/{}_img.jpg'.format(ts), image)
     try:
-        answer1 = predict(image, model1, preprocess_img, label2word)
+        # answer1 = predict(image, model1, preprocess_img, label2word)
         answer2 = predict(image, model2, preprocess3_img, label2word3)
         # answer3 = predict(image, model3, preprocess3_img, label2word2)
-        # answer4 = predict(image, model4, preprocess3_img, label2word3)
+        answer4 = predict(image, model4, preprocess3_img, label2word3)
 
         pred_ans, pred_val = None, -1
-        answer_list = [answer1, answer2, answer3, answer4]
+        answer_list = [answer2, answer4]
         # weight = [2, 1, 1, 0.6]
-        weight = [1.5, 1]
-        for word in answer1.keys():
+        weight = [1.2, 1]
+        for word in answer2.keys():
             val = 0
             for a_id, ans in enumerate(answer_list):
                 val += ans[word] * weight[a_id]
@@ -252,7 +253,7 @@ def inference():
     server_timestamp = time.time()
     with open('./result/{}_img.txt'.format(ts), 'w') as f:
         f.write(answer)
-
+    print(answer)
     res =   jsonify({'esun_uuid': data['esun_uuid'],
                     'server_uuid': server_uuid,
                     'answer': answer,
